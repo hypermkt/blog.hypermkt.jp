5a581dccb56843c0cdb9b19a41d19aed6733bba6
Makoto Chiba <hypermkt@gmail.com>
2020-02-26 23:47:18 +0900

    Fix profile

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `hypermkt blog`,
+    author: `Makoto Chiba`,
+    description: `A web developer blog who love Ruby/PHP.`,
+    siteUrl: `https://blog.hypermkt.jp/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `hypermkt`,
     },
   },
   plugins: [
