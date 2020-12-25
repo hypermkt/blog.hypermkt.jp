4ddae449c2339135f84bf7919da805977519fa81
hypermkt <hypermkt@gmail.com>
2020-12-25 20:54:38 +0900

    Add google analytics setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-253451-9'
+      }
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
