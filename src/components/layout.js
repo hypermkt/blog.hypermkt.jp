1544ea817cef18547f92c12ad4b6c7487ceea409
hypermkt <hypermkt@gmail.com>
2026-01-03 09:28:03 +0900

    記事一覧と記事詳細でタイトルサイズを固定にした

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,51 +6,27 @@ import CategoryList from "./category-list"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
-  let header
-
-  if (location.pathname === rootPath) {
-    header = (
-      <h1
+  const header = (
+    <h3
+      style={{
+        ...scale(0.5),
+        fontFamily: `Montserrat, sans-serif`,
+        marginTop: 0,
+        marginBottom: 0,
+      }}
+    >
+      <Link
         style={{
-          ...scale(1.5),
-          marginBottom: 0,
-          marginTop: 0,
+          boxShadow: `none`,
+          textDecoration: `none`,
+          color: `inherit`,
         }}
+        to={`/`}
       >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <h3
-        style={{
-          fontFamily: `Montserrat, sans-serif`,
-          marginTop: 0,
-          marginBottom: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
-    )
-  }
+        {title}
+      </Link>
+    </h3>
+  )
   return (
     <div
       style={{
